715edffb67a021e98f5eeb8df876ca4d3d0d2a5c
Jinah Adam <jinahadam@gmail.com>
2019-03-27 21:16:05 +1100

    CatTableViewDataSource placeholder

---
+++ agl/DataSources/CatTableViewDataSource.swift
@@ -0,0 +1,28 @@
+//
+//  CatTableViewDataSource.swift
+//  agl
+//
+//  Created by Jinah Adam on 27/3/19.
+//  Copyright © 2019 Jinah Adam. All rights reserved.
+//
+
+import UIKit
+
+class CatTableViewDataSource: NSObject, UITableViewDataSource {
+
+    var isLoading = false
+
+    func loadData() {
+        isLoading = true
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+
+}
---
+++ aglTests/DataSources/CatTableViewDataSourceTests.swift
@@ -0,0 +1,23 @@
+//
+//  CatTableViewDataSourceTests.swift
+//  aglTests
+//
+//  Created by Jinah Adam on 27/3/19.
+//  Copyright © 2019 Jinah Adam. All rights reserved.
+//
+
+import XCTest
+@testable import agl
+
+class CatTableViewDataSourceTests: XCTestCase {
+
+    var datasource: CatTableViewDataSource!
+    override func setUp() {
+        datasource = CatTableViewDataSource()
+    }
+
+
+    func testInitiaState() {
+
+    }
+}
