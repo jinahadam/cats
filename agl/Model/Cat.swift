b4cf9e4fce540f9d78ee2906e752bbf83957dc64
Jinah Adam <jinahadam@gmail.com>
2019-03-27 21:05:11 +1100

    Added actual models for use in datasource

---
+++ agl/Model/Cat.swift
@@ -0,0 +1,14 @@
+//
+//  Cat.swift
+//  agl
+//
+//  Created by Jinah Adam on 27/3/19.
+//  Copyright © 2019 Jinah Adam. All rights reserved.
+//
+
+import Foundation
+
+struct Cat {
+    let name: String
+    let owner: Owner
+}
---
+++ agl/Model/Owner.swift
@@ -0,0 +1,20 @@
+//
+//  Owner.swift
+//  agl
+//
+//  Created by Jinah Adam on 27/3/19.
+//  Copyright © 2019 Jinah Adam. All rights reserved.
+//
+
+import Foundation
+
+enum Gender {
+    case male
+    case female
+}
+
+struct Owner {
+    let name: String
+    let gender: Gender
+}
+
